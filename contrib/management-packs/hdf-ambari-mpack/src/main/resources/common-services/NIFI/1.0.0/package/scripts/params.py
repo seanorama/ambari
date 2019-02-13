9f1e2bd5e10e3b215ab9ab301774bf1806627503
Sean Roberts <seanorama@users.noreply.github.com>
2019-02-13 12:45:15 +0000

    NIFI: Support Ambari Metrics Collector using https
    - add protocol detection to nifi params.py

--- contrib/management-packs/hdf-ambari-mpack/src/main/resources/common-services/NIFI/1.0.0/package/scripts/params.py
+++ contrib/management-packs/hdf-ambari-mpack/src/main/resources/common-services/NIFI/1.0.0/package/scripts/params.py
@@ -211,6 +211,10 @@ jdk64_home=config['hostLevelParams']['java_home']
 
 #autodetect ambari server for metrics
 if 'metrics_collector_hosts' in config['clusterHostInfo']:
+  if default("/configurations/ams-site/timeline.metrics.service.http.policy", "HTTP_ONLY") == "HTTPS_ONLY":
+    metric_collector_protocol = 'https'
+  else:
+    metric_collector_protocol = 'http'
   metrics_collector_host = str(config['clusterHostInfo']['metrics_collector_hosts'][0])
   metrics_collector_port = str(get_port_from_url(config['configurations']['ams-site']['timeline.metrics.service.webapp.address']))
 else:
